382299c52b82a1836123c62051c9fd57dcfe30e4
g964526034 <964526034@qq.com>
2019-03-27 10:32:44 +0800

    添加项目

---
+++ src/main/java/com/neuedu/olms/controller/TeacherController.java
@@ -0,0 +1,60 @@
+package com.neuedu.olms.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.neuedu.olms.po.Teacher;
+import com.neuedu.olms.service.TeacherService;
+import com.neuedu.olms.util.C;
+import com.neuedu.olms.web.ApiResult;
+
+@RestController
+@RequestMapping("/teacher")
+public class TeacherController {
+	@Autowired
+	private TeacherService teacherService;
+	
+	@GetMapping("/test")
+	public String test() {
+		return "Hello World";
+	}
+	
+	/**
+	 * 验证号码是否存在，存在返回true，否则返回false
+	 * @param tel
+	 * @return
+	 */
+	@GetMapping("/validTel/{tel}")
+	public ApiResult hasExistTel(@PathVariable("tel")String tel) {
+		boolean rs = teacherService.hasTelExists(tel);
+		return ApiResult.build().setData(rs);
+	}
+	
+	@PostMapping("/add")
+	public ApiResult addTeacher(@Valid Teacher teacher,BindingResult bingResult) {
+
+		teacherService.addTeacher(teacher);
+		return ApiResult.build();
+	}
+	
+	@PostMapping("/login")
+	public ApiResult login(HttpSession session,@RequestParam("tel") String tel,@RequestParam("pwd") String pwd){
+		Teacher teacher = teacherService.login(tel, pwd);
+		session.setAttribute(C.USER_FLAG, teacher);
+		Map<String,String> rs = new HashMap<String,String>();
+		rs.put("token", teacher.getTokenEntity().getToken());
+		return ApiResult.build().setData(rs);
+	}
+}
---
+++ src/main/java/com/neuedu/olms/web/ExInfo.java
@@ -0,0 +1,20 @@
+package com.neuedu.olms.web;
+
+public class ExInfo {
+	public static int SUCCESS_CODE = 200;
+	public static String 	SUCCESS_MSG = "success";
+	
+	//请求错误参数
+	public static int PARAM_ERROR_CODE = 1000;
+	public static String PARAM_ERROR_MSG = "请求参数错误";
+	
+	//服务器端错误
+	public static int SERVER_ERROR_CODE = 500;
+	public static String SERVER_ERROR_MSG = "服务器端错误";
+
+	public static int USER_ACCOUNT_ERROR_CODE = 1001;
+	public static String USER_ACCOUNT_ERROR_MSG = "用户名或密码错误";
+
+	public static int USER_NO_LOGIN_CODE = 1002;
+	public static String USER_NO_LOGIN_MSG = "用户未登录";
+}
